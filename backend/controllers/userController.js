37f5d761840a337281f2614737a5e9dfe708f9f9
Mohammad Arbaab <kazmiwrite@gmail.com>
2025-07-01 20:17:22 -0400

    add cookie parser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,12 @@
 const asyncHandler = require('express-async-handler')
 const User = require("../models/userModels");
+const jwt = require("jsonwebtoken");
 
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" })
+}
 
-const registerUser = asyncHandler(async(req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
     console.log("req.body:", req.body);
 
@@ -19,7 +23,7 @@ const registerUser = asyncHandler(async(req, res) => {
     }
 
     // check if user already exist
-    const userExist =await User.findOne({ email });
+    const userExist = await User.findOne({ email });
 
     if (userExist) {
         res.status(400);
@@ -27,19 +31,33 @@ const registerUser = asyncHandler(async(req, res) => {
     }
 
 
+
     // create new user
-    const user =await User.create({
+    const user = await User.create({
         name,
         email,
         password
     });
 
+    
+    // Genereate Token
+    const token = generateToken(user._id);
+
+
+    // send HTTP only cookie
+    res.cookie("token",token,{
+        path:"/",
+        httpOnly:true,
+        expires:new Date(Date.now() + 1000 * 86400), 
+        sameSite:"none",
+        secure:true
+    })
     if (user) {
         const { _id, name, email, photo, phone, bio } = user
         res.status(201).json({
-            _id, name, email, photo, phone, bio
+            _id, name, email, photo, phone, bio,token
         })
-    }else{
+    } else {
         res.status(400);
         throw new Error("Invalid user Data");
     }
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute");
 const errorHandler = require("./middleware/errorMiddleware");
+const cookieParser=require("cookie-parser");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json()); // ✅ Parse JSON bodies
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false })); // ✅ Parse URL-encoded form data
 
 // Routes
